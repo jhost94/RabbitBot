02f4509f33862cae38f4d7e64eeae417816e006c
FabioCardoso <jhost94@gmail.com>
2020-12-03 22:24:26 -0100

    added config.js and added some changes in app.html

---
+++ config.js
@@ -0,0 +1,10 @@
+const botName = "princesspotato_bot";
+
+const channel = ["jhost94"];
+
+//OAuth code for the "botUser". Must be logged into "botUser" account when going to the link below
+//Can be obtained at https://twitchapps.com/tmi/
+//Everything after "oauth:" should be put into the "" below. Ex. oauth:abc123 would mean abc123 goes in the quotes
+const oauth = "611w4kmblmhoebf483dkm29zwv4pax";
+
+const baseCommand = "!rabbit";
