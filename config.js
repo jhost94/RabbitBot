c307479c02067ba155444fe7d2ca7c63d7a36f54
Jhost <jhost94@gmail.com>
2020-12-22 20:17:10 -0100

    Changed config.js to be customized in the future

--- config.js
+++ config.js
@@ -1,12 +1,14 @@
-//const botName = "jhost_bot";
+//The bot twitch ID/name
 const botName = "princesspotato_bot";
 
-const channel = ["jhost94"];
+
+//The channel name where you want the bot to listen
+const channel = [""];
 
 //OAuth code for the "botUser". Must be logged into "botUser" account when going to the link below
 //Can be obtained at https://twitchapps.com/tmi/
 //Everything after "oauth:" should be put into the "" below. Ex. oauth:abc123 would mean abc123 goes in the quotes
-//const oauth = "gwgp1vjf6gqqt2bl1g75fpkh4jd4ph";
-const oauth = "611w4kmblmhoebf483dkm29zwv4pax";
+
+const oauth = "";
 
 const baseCommand = "!rb";
